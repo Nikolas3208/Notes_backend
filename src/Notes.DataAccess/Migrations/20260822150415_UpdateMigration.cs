13b0e97dad98583d83af2478d8fa5d165ca59a93
Boghdna <haharin06@gmail.com>
2026-08-22 18:05:44 +0300

    upgrade migration

---
+++ src/Notes.DataAccess/Migrations/20260822150415_UpdateMigration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Notes.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Users",
+                newName: "LastName");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "LastName",
+                table: "Users",
+                newName: "Name");
+        }
+    }
+}
